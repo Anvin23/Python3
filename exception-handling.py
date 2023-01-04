c688a64eba9e6ed642c19a3fa14f0111f315e8f3
Anvin.C.Shaju <anvincsg@gmail.com>
2023-01-04 10:17:57 +0530

    added 'finally' in exception program

--- exception-handling.py
+++ exception-handling.py
@@ -10,6 +10,8 @@ except :
     # we can also write 'except Exception as e'
     print("Invalid input")
 
+print("---------------------")
+
 
 # we can also handle specific and multiple errors
 try:
@@ -17,10 +19,35 @@ try:
     a = [2,3,4]
     print(a[num])
 except ValueError:
-    print("Number entered is not an integer !!!")
+    print("Input given is not an integer !!!")
 except IndexError:
     print("Index out of bounds !!!")
+
+finally:
+    print("I am always executed")
 
 print("Some important code")
 
 print("The end")
+
+print("---------------------")
+
+def func():
+    list = [1,2,3]
+    n = int(input("Enter a number : "))
+    try:
+        print(list[n])
+        return 1
+    except:
+        print("Error occured")
+        return 0
+    
+    finally:
+        print("I am inevitable")
+        # this will always run even if it comes after the return statements
+
+    print("Remaining part of function")
+    # since both try and except has a return statement , this print will not run
+
+x = func()
+print(x)
